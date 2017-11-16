97e9423d39a0378c2e028e0d3e6e04a1608a3250
Sanjeev Singh <sanjeevhbti45@gmail.com>
2017-11-16 23:21:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,9 +53,9 @@ def webhook():
 def processRequest(req):
     if req.get("result").get("action") != "yahooWeatherForecast":
         return {}
-    #baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
     
-    
+    '''
     baseurl = "https://flight.yatra.com/air-service/dom2/search?type=O&viewName=normal&flexi=0&noOfSegments=1&origin=DEL&originCountry=IN&destination=BLR&destinationCountry=IN&flight_depart_date=18/11/2017&ADT=1&CHD=0&INF=0&class=Economy&source=fresco-homeUrl"
     yql_url = baseurl + "&format=json"
     result = urlopen(yql_url).read()
@@ -64,7 +64,7 @@ def processRequest(req):
     #data['airportName']['DEL']
     res = makeWebhookResult_1(data['airportName']['DEL'])
     
-    #res = makeWebhookResult_1(data)
+    res = makeWebhookResult_1(data)
     return res
      
     '''
@@ -76,7 +76,7 @@ def processRequest(req):
     data = json.loads(result)
     res = makeWebhookResult_1(data)
     return res
-    '''
+    
 
 def makeWebhookResult_1(data1):
     #speech = data

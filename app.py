adafa0a8ca5260ec75f64c0620b35236ce2f3f6f
Sanjeev Singh <sanjeevhbti45@gmail.com>
2017-11-17 01:03:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,12 +53,15 @@ def webhook():
 def processRequest(req):
     if req.get("result").get("action") != "yahooWeatherForecast":
         return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    #baseurl = "https://query.yahooapis.com/v1/public/yql?"
     
-    '''
-    baseurl = "https://flight.yatra.com/air-service/dom2/search?"
-    yql_query = "type=O&viewName=normal&flexi=0&noOfSegments=1&origin=DEL&originCountry=IN&destination=BLR&destinationCountry=IN&flight_depart_date=21/11/2017&ADT=1&CHD=0&INF=0&class=Economy&source=fresco-homeUrl"
-    yql_url = baseurl + urlencode({yql_query}) + "&format=json"
+    
+    baseurl = "https://flight.yatra.com/air-service/dom2/search?type=O&viewName=normal&flexi=0&noOfSegments=1&origin=DEL&originCountry=IN&destination=BLR&destinationCountry=IN&flight_depart_date=21/11/2017&ADT=1&CHD=0&INF=0&class=Economy&source=fresco-homeUrl"
+    
+#     yql_query = "type=O&viewName=normal&flexi=0&noOfSegments=1&origin=DEL&originCountry=IN&destination=BLR&destinationCountry=IN&flight_depart_date=21/11/2017&ADT=1&CHD=0&INF=0&class=Economy&source=fresco-homeUrl"
+    
+    yql_query = makeYqlQuery(req)
+    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
     result = urlopen(yql_url).read()
     data = json.loads(result)
     
@@ -70,7 +73,6 @@ def processRequest(req):
      
     '''
     yql_query = makeYqlQuery(req)
-    #yql_query = ""
     if yql_query is None:
         return {}
     yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
@@ -78,7 +80,7 @@ def processRequest(req):
     data = json.loads(result)
     res = makeWebhookResult_1(data)
     return res
-    
+    '''
     
 
 def makeWebhookResult_1(data1):

2d2e8cc94fd84c3d0c8cd9f67c11b41569f7a602
Sanjeev Singh <sanjeevhbti45@gmail.com>
2017-11-17 11:37:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,8 +58,8 @@ def processRequest(req):
     
     baseurl = "https://flight.yatra.com/air-service/dom2/search?type=O&viewName=normal&flexi=0&noOfSegments=1&origin=DEL&originCountry=IN&destination=BLR&destinationCountry=IN&flight_depart_date=18/11/2017&ADT=1&CHD=0&INF=0&class=Economy&source=fresco-homeUrl"
     yql_url = baseurl + "&format=json"
-    result = urlopen(yql_url).read()
-#     data = json.loads(result)
+#     result = urlopen(yql_url).read()
+    data = json.loads(result)
     
 
 #     yql_query = makeYqlQuery(req)

216c60d36946ef3a32c00f6f6302657304142946
Sanjeev Singh <sanjeevhbti45@gmail.com>
2017-11-17 12:06:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,10 +56,12 @@ def processRequest(req):
     #baseurl = "https://query.yahooapis.com/v1/public/yql?"
     
     
-    baseurl = "https://flight.yatra.com/air-service/dom2/search?type=O&viewName=normal&flexi=0&noOfSegments=1&origin=DEL&originCountry=IN&destination=BLR&destinationCountry=IN&flight_depart_date=18/11/2017&ADT=1&CHD=0&INF=0&class=Economy&source=fresco-homeUrl"
+#     baseurl = "https://flight.yatra.com/air-service/dom2/search?type=O&viewName=normal&flexi=0&noOfSegments=1&origin=DEL&originCountry=IN&destination=BLR&destinationCountry=IN&flight_depart_date=18/11/2017&ADT=1&CHD=0&INF=0&class=Economy&source=fresco-homeUrl"
+    
+    baseurl = "https://maps.googleapis.com/maps/api/place/search/json?location:33.787794,-117.853111"
     yql_url = baseurl + "&format=json"
-#     result = urlopen(yql_url).read()
-#     data1 = json.loads(result)
+    result = urlopen(yql_url).read()
+    data1 = json.loads(result)
     
 
 #     yql_query = makeYqlQuery(req)
